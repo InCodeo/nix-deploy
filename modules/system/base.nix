bf8f115cabce34297f2887c82e6e21f787787a23
InCodeo <githublife@proton.me>
2024-11-26 16:17:50 +1100

    Update base.nix

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -3,84 +3,78 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Basic system configuration
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Basic networking
+  # Basic networking configuration
   networking = {
+    hostName = "nixos-manager";  # You can override this in specific configs
     useDHCP = true;
     firewall = {
       enable = true;
-      # Allow Tailscale
-      allowedUDPPorts = [ config.services.tailscale.port ];
-      # Required for Tailscale to work properly
+      allowedTCPPorts = [ 
+        22    # SSH
+      ];
+      allowedUDPPorts = [ 
+        config.services.tailscale.port
+      ];
       trustedInterfaces = [ "tailscale0" ];
     };
   };
 
   # Essential system packages
   environment.systemPackages = with pkgs; [
-    # System utilities
-    wget
-    curl
-    vim
-    git
-    htop
-    tmux
+    # Core utilities
+    wget vim git curl
     
-    # Networking tools
-    inetutils
-    netcat
-    bind # for dig
+    # Network tools
     tailscale
+    bind        # for dig/nslookup
+    inetutils   # for ping, etc.
     
+    # System monitoring
+    htop tmux
+    
+    # Shell
+    zsh oh-my-zsh
+
     # Docker requirements
     docker-compose
-
-    # Monitoring
-    bottom # Better system monitor
-    ctop   # Container monitoring
+    ctop
   ];
+
+  # Enable Tailscale
+  services.tailscale.enable = true;
 
   # SSH Configuration
   services.openssh = {
     enable = true;
     settings = {
-      PasswordAuthentication = true;  # Consider disabling after initial setup
-      PermitRootLogin = "prohibit-password";
+      PasswordAuthentication = true;  # Will be disabled after initial setup
+      PermitRootLogin = "yes";       # Will be disabled after initial setup
       X11Forwarding = false;
       PermitEmptyPasswords = false;
       MaxAuthTries = 3;
     };
   };
 
-  # Enable Tailscale
-  services.tailscale.enable = true;
-
-  # Resource monitoring service
-  systemd.services.resource-monitor = {
-    description = "System Resource Monitor";
-    script = ''
-      memory_total=$(free -m | awk '/^Mem:/{print $2}')
-      if [ "$memory_total" -lt 2048 ]; then
-        echo "Warning: System has less than 2GB RAM (${memory_total}MB)"
-        exit 1
-      fi
-      
-      # Check disk space
-      disk_free=$(df -m /var/lib | awk 'NR==2 {print $4}')
-      if [ "$disk_free" -lt 5120 ]; then
-        echo "Warning: Less than 5GB free space available"
-        exit 1
-      fi
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    wantedBy = [ "multi-user.target" ];
+  # User Configuration
+  users.users.admin = {
+    isNormalUser = true;
+    description = "Management Admin";
+    extraGroups = [ "wheel" "docker" ];
+    initialPassword = "changeme";
   };
+
+  # Shell Configuration
+  programs.zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "docker" "sudo" ];
+      theme = "robbyrussell";
+    };
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+  users.users.root.shell = pkgs.zsh;
 
   # Docker base configuration
   virtualisation.docker = {
@@ -90,25 +84,6 @@
       dates = "weekly";
     };
   };
-
-  # User configuration
-  users.users.admin = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
-    openssh.authorizedKeys.keys = []; # Add your SSH keys here
-    initialPassword = "changeme";
-  };
-
-  # Shell configuration
-  programs.zsh = {
-    enable = true;
-    ohMyZsh = {
-      enable = true;
-      plugins = [ "git" "docker" "docker-compose" ];
-      theme = "robbyrussell";
-    };
-  };
-  users.defaultUserShell = pkgs.zsh;
 
   # System limits for Docker
   boot.kernel.sysctl = {
